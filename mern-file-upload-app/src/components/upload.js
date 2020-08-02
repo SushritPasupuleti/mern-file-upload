41555edea950170684cff49b534d9f83061e8e4d
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-02 17:46:22 +0530

    Added API call from upload component and fixed casing

--- mern-file-upload-app/src/components/upload.js
+++ mern-file-upload-app/src/components/upload.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import axios from 'axios';
 
-export default function upload() {
+export default function Upload(props) {
+
+    const [profileImg, setProfileImg] = React.useState("");
+
+    const onFileChange = (e) => {
+        setProfileImg(e.target.files[0])
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('profileImg', profileImg)
+        axios.post("http://localhost:4000/api/user-profile", formData, {
+        }).then(res => {
+            console.log(res)
+        })
+    }
+
     return (
         <div className="container">
-            <div className="row">
-                <form>
-                    <h3>React File Upload</h3>
-                    <div className="form-group">
-                        <input type="file" />
-                    </div>
-                    <div className="form-group">
-                        <button className="btn btn-primary" type="submit">Upload</button>
-                    </div>
-                </form>
+                <div className="row">
+                    <form onSubmit={onSubmit}>
+                        <div className="form-group">
+                            <input type="file" onChange={onFileChange} />
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary" type="submit">Upload</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
     )
 }
