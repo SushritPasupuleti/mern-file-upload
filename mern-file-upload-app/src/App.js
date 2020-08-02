2687814f9963aeb1a92c48622501fdfdf6f2d9db
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-02 17:38:44 +0530

    Added Upload Component to App

--- mern-file-upload-app/src/App.js
+++ mern-file-upload-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Upload from './components/upload';
 
 function App() {
   return (
     <div className="App">
       <h2>React File Upload Demo</h2>
+      <Upload></Upload>
     </div>
   );
 }
